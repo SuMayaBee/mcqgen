82cd3933be624b4b89bb86784efd7ff91f5860de
SuMayaBee <sumaiyaislam-14-2021711385@it.du.ac.bd>
2024-03-26 01:01:40 +0600

    file changes

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='Sumaiya Islam',
+    author_email='sumaiyaislam-14-2021711385@it.du.ac.bd',
+    install_requires=["openai", "langchain", "streamlit", "python-dotenv", "PyPDF2"],
+    packages=find_packages()
+)
+
+
+
+
+
+### conda activate /home/su_maya/Desktop/mcqgen/env
+
+
